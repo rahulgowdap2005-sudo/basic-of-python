02a98271d347995e6d0d09df18b8458b74af967c
rahulgowdap2005-sudo <rahulgowdap2005@gmail.com>
2026-09-15 23:39:46 +0530

    Add basic arithmetic operations in operator.py

---
+++ operator.py
@@ -0,0 +1,13 @@
+# Python Operators
+
+a = 10
+b = 3
+
+# Arithmetic operators
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Floor Division:", a // b)
+print("Modulus:", a % b)
+print("Power:", a ** b)
